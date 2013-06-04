a1a186b68868c0c84a156373c2662e4051b9d4b7
Claudia Farias <krawdyah@gmail.com>
2013-06-04 03:59:59 -0300

    tenant concern

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,9 @@
 class Project
   include Mongoid::Document
+  include Tenant
   field :name, type: String
   field :description, type: String
   belongs_to :company
   has_many :rosters
-  validates :name, presence: true
+  validates :name, :company_id, presence: true
 end
