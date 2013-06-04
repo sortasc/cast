aaaf0e18b3018c05311fc45e12bbe2f592653b8a
Claudia Farias <krawdyah@gmail.com>
2013-06-04 04:24:21 -0300

    custom registration devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cast::Application.routes.draw do
 
-	devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+	devise_for :users, controllers: { registrations: 'registrations' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 	devise_for :roots, controllers: { sessions: "zone/sessions" }, path: '', path_names: { sign_in: 'zone/login', sign_out: 'zone/logout' }
   
   namespace :zone do
