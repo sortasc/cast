ec9cb2f46655429a8692902e329e02ec5bfbf44a
Claudia Farias <krawdyah@gmail.com>
2013-06-04 04:00:12 -0300

    current_tenant helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  around_filter :scope_current_tenant
   before_filter :set_locale
 	layout :layout_devise
 
@@ -13,4 +14,18 @@ class ApplicationController < ActionController::Base
   	devise_controller? ? 'login' : 'application'
 	end
 
+	private
+
+  def current_tenant
+    Company.find_by subdomain: request.subdomain
+  end
+  helper_method :current_tenant
+
+  def scope_current_tenant
+    Company.current_id = current_tenant.id
+    yield
+  ensure
+    Company.current_id = nil
+  end
+  
 end
